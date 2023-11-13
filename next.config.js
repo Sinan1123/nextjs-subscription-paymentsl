30f991ce9b137d4a4cf84ad2b6321da4550b475e
sinan gürses <95588874+Sinan1123@users.noreply.github.com>
2023-11-13 22:36:27 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    serverActions: true
-  }
-};
+const { execSync } = require('child_process');
 
-module.exports = nextConfig;
+module.exports = {
+  // Diğer konfigürasyonlar...
+
+  webpack: (config, { isServer }) => {
+    // Sadece sunucu tarafında çalıştırın
+    if (isServer) {
+      try {
+        // Prisma Client'i yeniden oluşturun
+        execSync('prisma generate');
+      } catch (error) {
+        console.error('Prisma Client oluşturulurken bir hata oluştu:', error);
+        process.exit(1); // Hata durumunda build işlemini durdurun
+      }
+    }
+
+    return config;
+  },
+};
